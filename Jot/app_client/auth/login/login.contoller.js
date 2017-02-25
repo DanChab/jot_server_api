62aa4f1cdd3b26fb33eb479018b87a71f3d96246
Danchab <banchabala@gmail.com>
2017-02-25 18:30:23 +0200

    Update Angular route protection

---
+++ Jot/app_client/auth/login/login.contoller.js
@@ -0,0 +1,27 @@
+(function(){
+
+    angular
+    .module('jot')
+    .controller('loginCtrl', loginCtrl);
+
+    loginCtrl.$inject = ['$location', 'authentication'];
+    function loginCtrl($location, authentication){
+    var vm = this;
+
+    vm.credentials = {
+        email : "",
+        password : ""
+    };
+
+    vm.onSubmit = function(){
+        authentication
+        .login(vm.credentials)
+        .error(function(err){
+            alert(err);
+        })
+        .then(function(){
+            $location.path('profile');
+        });
+    };
+}
+});
