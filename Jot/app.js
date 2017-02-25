0d6efddf6667f3b9c1617a8d3c5a769b4c8aa6ee
Danchab <banchabala@gmail.com>
2017-02-25 08:16:30 +0200

    update app.js with comments

--- Jot/app.js
+++ Jot/app.js
@@ -4,7 +4,16 @@ var favicon = require('server-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// [SH] Require Passport
 var passport = require('passport');
 
+// [SH] Bring in the data model
+require('./app_api/models/db');
+// [SH] Bring in the Passport config after model is defined
+require('./app_api/config/passport');
+
+// [SH] Initialise Passport before using the route middleware
 app.use(passport.initialize());
+
+// [SH] Use the API routes when path starts with /api
 app.use('/api', routesApi);
